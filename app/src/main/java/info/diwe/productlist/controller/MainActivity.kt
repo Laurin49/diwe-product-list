b6e532e319e57ee152c0d5f716fcde46096b610a
Laurin49 <welter@diwe.info>
2021-01-12 17:22:16 +0100

    Changes to Lession 03

--- app/src/main/java/info/diwe/productlist/controller/MainActivity.kt
+++ app/src/main/java/info/diwe/productlist/controller/MainActivity.kt
@@ -4,21 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import info.diwe.productlist.R
+import info.diwe.productlist.adapters.CategoryAdapter
 import info.diwe.productlist.model.Category
 import info.diwe.productlist.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(
+        adapter = CategoryAdapter(
             this,
-            android.R.layout.simple_list_item_1,
             DataService.categories)
 
         categoryListView.adapter = adapter
