b653dd9cda31ede9897c280f71a592c039b4deb7
Laurin49 <welter@diwe.info>
2021-01-13 11:27:21 +0100

    Changes to RecyclerView ...

--- app/src/main/java/info/diwe/productlist/controller/MainActivity.kt
+++ app/src/main/java/info/diwe/productlist/controller/MainActivity.kt
@@ -3,24 +3,29 @@ package info.diwe.productlist.controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import info.diwe.productlist.R
 import info.diwe.productlist.adapters.CategoryAdapter
+import info.diwe.productlist.adapters.CategoryRecycleAdatper
 import info.diwe.productlist.model.Category
 import info.diwe.productlist.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdatper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(
-            this,
-            DataService.categories)
+        adapter = CategoryRecycleAdatper(this, DataService.categories)
 
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 }
